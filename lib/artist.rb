9b1d4959d94a7e5ff2dacc0953147e2243880808
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-11-05 00:41:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,13 +7,15 @@ class Artist
     @songs = []
   end
   
+   def add_song(song)
+    @songs << song
+  end
+  
   def songs
     @songs
   end
   
-  def add_song(song)
-    @songs << song
-  end
+ 
   
   def add_song_by_name(name)
     
